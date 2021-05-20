83814eaca627dda2cf47fafb9a5f68ea73204c9d
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2021-05-20 21:36:37 +0200

    Updated Flutter Maps screen to include an app bar.

--- lib/fluttermaps.dart
+++ lib/fluttermaps.dart
@@ -7,26 +7,29 @@ class FlutterMaps extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new FlutterMap(
-      options: new MapOptions(
-        zoom: 13.0,
-      ),
-      layers: [
-        new TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c']),
-        new MarkerLayerOptions(
-          markers: [
-            new Marker(
-              width: 80.0,
-              height: 80.0,
-              builder: (ctx) => new Container(
-                child: new FlutterLogo(),
-              ),
+    return new Scaffold(
+        appBar: AppBar(title: Text("Flutter Map")),
+        body: new FlutterMap(
+          options: new MapOptions(
+            zoom: 13.0,
+          ),
+          layers: [
+            new TileLayerOptions(
+                urlTemplate:
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c']),
+            new MarkerLayerOptions(
+              markers: [
+                new Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  builder: (ctx) => new Container(
+                    child: new FlutterLogo(),
+                  ),
+                ),
+              ],
             ),
           ],
-        ),
-      ],
-    );
+        ));
   }
 }
