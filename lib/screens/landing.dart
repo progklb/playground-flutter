25456bbc9631b2eb40add8b99feaecef03149cd9
Kevin Baynham <kevin.baynham.k7@gmail.com>
2021-06-06 21:40:22 +0200

    Migrated the creation of the landing screen entries into their own function.

--- lib/screens/landing.dart
+++ lib/screens/landing.dart
@@ -7,6 +7,13 @@ import 'package:flutter_playground/screens/fileStorage.dart';
 class Landing extends StatelessWidget {
   static const String route = 'landing';
 
+  Widget createEntry(BuildContext context, {String title, Function onPressed}) {
+    return TextButton(
+      child: Text("$title"),
+      onPressed: onPressed,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,28 +31,18 @@ class Landing extends StatelessWidget {
 
         // Main content
         body: ListView(children: [
-          TextButton(
-            child: Text("Startup Name Generator"),
-            onPressed: () {
-              Navigator.pushNamed(context, "/${RandomWords.route}");
-            },
-          ),
-          TextButton(
-            child: Text("FL Chart"),
-            onPressed: () {
-              Navigator.pushNamed(context, "/${FLChart.route}");
-            },
-          ),
-          TextButton(
-              child: Text("Flutter Map"),
-              onPressed: () {
-                Navigator.pushNamed(context, "/${FlutterMaps.route}");
-              }),
-          TextButton(
-              child: Text("File Storage"),
-              onPressed: () {
-                Navigator.pushNamed(context, "/${FileStorage.route}");
-              })
+          createEntry(context, title: "Startup Name Generator", onPressed: () {
+            Navigator.pushNamed(context, "/${RandomWords.route}");
+          }),
+          createEntry(context, title: "FL Chart", onPressed: () {
+            Navigator.pushNamed(context, "/${FLChart.route}");
+          }),
+          createEntry(context, title: "Flutter Map", onPressed: () {
+            Navigator.pushNamed(context, "/${FlutterMaps.route}");
+          }),
+          createEntry(context, title: "File Storage", onPressed: () {
+            Navigator.pushNamed(context, "/${FileStorage.route}");
+          }),
         ]));
   }
 }
