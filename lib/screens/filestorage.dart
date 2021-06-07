4b5a9457bf76687d33a5baf0ad8ae5852f85cc72
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2021-06-07 21:01:22 +0200

    Added saving/loading of the user using shared preferences (note that this is not working, but has been left in place to demonstrate the concept).

--- lib/screens/filestorage.dart
+++ lib/screens/filestorage.dart
@@ -2,8 +2,12 @@
 Demonstrates saving/loading data to and from a JSON file.
 */
 
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FileStorage extends StatefulWidget {
   static const String route = 'fileStorage';
@@ -18,11 +22,30 @@ class _FileStorageState extends State<FileStorage> {
   /// The model that is storing our user data.
   User userModel;
 
+  // FIX Saving/loading does not work - shared preferences fails due to error:
+  // Error: MissingPluginException(No implementation found for method getAll on channel plugins.flutter.io/shared_preferences)
+
   /// Saves the current user details as a JSON object to storage.
-  void saveUser() {}
+  void saveUser() async {
+    // Using preferences here because web apps don't have access to file system.
+    var prefs = await SharedPreferences.getInstance();
+    var userJson = jsonEncode(userModel.toJson());
+    prefs.setString("user", userJson);
+
+    print("User object saved:\n" + userJson);
+  }
 
   /// Loads the stored JSON user details and populates the UI.
-  void loadUser() {}
+  void loadUser() async {
+    var prefs = await SharedPreferences.getInstance();
+    var userJson = prefs.getString("user");
+    userModel = jsonDecode(userJson);
+
+    print("User object loaded:\n" + userJson);
+
+    // Refresh the screen.
+    setState(() {});
+  }
 
   Widget createTextEntry(
       BuildContext context, String placeholderText, String currentText) {
