421e14781d9dda7d3cca4ba6b1b576195fb43fb0
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2021-06-07 16:13:19 +0200

    Updated the file storage experiment so that the user interface now has padding and a more reusable implementation for creating / assigning text entries by using a user model. (Model loading/saving still to come)

--- lib/screens/filestorage.dart
+++ lib/screens/filestorage.dart
@@ -3,6 +3,7 @@ Demonstrates saving/loading data to and from a JSON file.
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/models/user.dart';
 
 class FileStorage extends StatefulWidget {
   static const String route = 'fileStorage';
@@ -14,23 +15,45 @@ class FileStorage extends StatefulWidget {
 }
 
 class _FileStorageState extends State<FileStorage> {
-  TextButton saveButton = TextButton(child: Text("Save"), onPressed: () => {});
-  TextButton loadButton = TextButton(child: Text("Load"), onPressed: () => {});
+  /// The model that is storing our user data.
+  User userModel;
 
-  Widget createTextEntry(BuildContext context, String placeholderText) {
-    return TextField(decoration: InputDecoration(labelText: placeholderText));
+  /// Saves the current user details as a JSON object to storage.
+  void saveUser() {}
+
+  /// Loads the stored JSON user details and populates the UI.
+  void loadUser() {}
+
+  Widget createTextEntry(
+      BuildContext context, String placeholderText, String currentText) {
+    // Create a text controller so that we can set the text.
+    // Note that we manually move the position of the cursor to the end of the field.
+    var textController = new TextEditingController();
+    textController.value = TextEditingValue(
+        text: currentText,
+        selection: TextSelection.fromPosition(
+            TextPosition(offset: currentText.length)));
+
+    return Padding(
+        padding: EdgeInsets.all(10),
+        child: TextField(
+            controller: textController,
+            decoration: InputDecoration(labelText: placeholderText)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("File Storage")),
-      body: Column(children: [
-        createTextEntry(context, "First name(s)"),
-        createTextEntry(context, "Surname"),
-        Divider(height: 50, color: Colors.blue),
-        Row(children: [saveButton, loadButton])
-      ]),
-    );
+        appBar: AppBar(title: Text("File Storage")),
+        body: Column(children: [
+          createTextEntry(context, "First Name(s)", userModel?.firstName ?? ""),
+          createTextEntry(context, "Surname", userModel?.surname ?? ""),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(children: [
+                TextButton(child: Text("Save"), onPressed: saveUser),
+                TextButton(child: Text("Load"), onPressed: loadUser)
+              ])),
+        ]));
   }
 }
